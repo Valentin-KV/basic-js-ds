825101b6f2b96c70c68a68bc09e0e585049d514e
Valentin-KV <krivenko.vg@yandex.ru>
2025-11-05 20:02:47 +0300

    feat: add solution queue task

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,19 +13,45 @@ const { NotImplementedError } = require('../lib/errors');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-  getUnderlyingList() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  getUnderlyingList() {
+    return this._toPlain(this.head);
+  }
+
+  enqueue(value) {
+    const node = new ListNode(value);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+      return;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   }
 
   dequeue() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (this.head === null) {
+      return undefined;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    if (this.head === null) {
+      this.tail = null;
+    }
+    return value;
+  }
+
+  _toPlain(node) {
+    if (node === null) return null;
+    return {
+      value: node.value,
+      next: this._toPlain(node.next)
+    };
   }
 }
 
